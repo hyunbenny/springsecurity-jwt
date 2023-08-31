12e4d92607f7f37a76e57d62e40af3376e67b848
hyunbenny <bluesky3268@naver.com>
2023-08-31 10:43:08 +0900

    프로젝트 기본 세팅 및 테스트 완료

---
+++ src/main/java/com/hyunbenny/springsecurityjwt/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.hyunbenny.springsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "<h1>HOME</h1>";
+    }
+
+}
